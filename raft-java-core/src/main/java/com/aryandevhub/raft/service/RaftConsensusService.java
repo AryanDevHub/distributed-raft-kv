2055b201f9d4d776c65c703e979f492e5046c81e
AryanDevHub <dandotiyaaryan2010@gmail.com>
2026-09-26 00:26:54 +0530

    refactor: modernize to Java 17 and migrate namespace to com.aryandevhub.raft

---
+++ raft-java-core/src/main/java/com/aryandevhub/raft/service/RaftConsensusService.java
@@ -0,0 +1,18 @@
+package com.aryandevhub.raft.service;
+
+import com.aryandevhub.raft.proto.RaftProto;
+
+/**
+ * raft节点之间相互通信的接口。
+ * 
+ */
+public interface RaftConsensusService {
+
+    RaftProto.VoteResponse preVote(RaftProto.VoteRequest request);
+
+    RaftProto.VoteResponse requestVote(RaftProto.VoteRequest request);
+
+    RaftProto.AppendEntriesResponse appendEntries(RaftProto.AppendEntriesRequest request);
+
+    RaftProto.InstallSnapshotResponse installSnapshot(RaftProto.InstallSnapshotRequest request);
+}
---
+++ raft-java-example/src/main/java/com/aryandevhub/raft/example/server/service/ExampleService.java
@@ -0,0 +1,11 @@
+package com.aryandevhub.raft.example.server.service;
+
+/**
+ * 
+ */
+public interface ExampleService {
+
+    ExampleProto.SetResponse set(ExampleProto.SetRequest request);
+
+    ExampleProto.GetResponse get(ExampleProto.GetRequest request);
+}
